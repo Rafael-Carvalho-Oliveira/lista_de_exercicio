eca2beb18e9d6deadba79f5e1470554cb5199763
tiagovinicius10 <168542213+tiagovinicius10@users.noreply.github.com>
2024-08-27 11:31:59 -0300

    atv8.py

--- atv8.py
+++ atv8.py
@@ -2,8 +2,8 @@ n1 = input('digite o primeiro numero: ')
 n1 = int(n1)
 n2 = input('digite o segundo numero: ')
 n2 = int(n2)
-n3 = input('digite o terceiro numero: ')
+n3 = input('digite o terceiro numero: ') #solicitando o numero para o usuario
 n3 = int(n3)
 
-media = (n1 + n2 + n3)/3
-print(f'o valor da media entre esses tres numeros sera: {media:.2f}')
+media = (n1 + n2 + n3)/3 #calculando a media
+print(f'o valor da media entre esses tres numeros sera: {media:.2f}') #devolvendo a informação
