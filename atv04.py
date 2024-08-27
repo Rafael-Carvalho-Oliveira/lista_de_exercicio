eb3dbdf983aaf68da14d414f680b654528d005c2
joaopqua <168540452+joaopqua@users.noreply.github.com>
2024-08-27 11:31:55 -0300

    atv04.py

--- atv04.py
+++ atv04.py
@@ -1,9 +1,8 @@
 n1 = input('digite um numero inteiro: ')
 n1 = int(n1)
-n2 = input('digite um numero inteiro: ')
+n2 = input('digite um numero inteiro: ')  #SOLICITANDO O NUMERO PARA O USUARIO
 n2 = int(n2)
 
-div = n1/n2
+div = n1/n2 #calculando a divisao dos numeros
 
-print(f'o resultado da divisão dos numeros que voce me infformou é {div}')
-
+print(f'o resultado da divisão dos numeros que voce me infformou é {div}') #devolvendo a informação
