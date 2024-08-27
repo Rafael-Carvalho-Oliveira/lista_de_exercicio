b5430fbe9af9c7487ed10a3c698b28c2ba6624ab
joaopqua <168540452+joaopqua@users.noreply.github.com>
2024-08-27 11:35:25 -0300

    atv07.py

--- atv07.py
+++ atv07.py
@@ -1,6 +1,7 @@
-dol = input('digite um valor em dolares: ')
-dol = float(dol)
-real = dol * 5.50  #
+dol = input('digite um valor em dolares: ') #solicitando o valor da variavel
+dol = float(dol) # convertendo o valor do dado
+real = dol * 5.50  #calculo de dol para real
 
-print(f'em real esse valor sera R${real:.2f}') 
+print(f'em real esse valor sera R${real:.2f}') # informando o valor e limitando as casas decimais
+
 
