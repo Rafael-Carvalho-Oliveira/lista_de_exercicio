cce08b47c3792d046b7994395807569fe405d22e
joaopqua <168540452+joaopqua@users.noreply.github.com>
2024-08-27 11:34:18 -0300

    atv05.py

--- atv05.py
+++ atv05.py
@@ -1,8 +1,9 @@
-n1 = input('digite um numero inteiro: ')
-n1 = float(n1)
+n1 = input('digite um numero inteiro: ') #solicitando o numero para o usuario
+n1 = float(n1) # convertendo o valor do dado para float 
 n2 = input('digite um numero inteiro: ')
 n2 = float(n2)
 
-div = n1/n2
+div = n1/n2  # calculando a divisao dos numeros
 
 print(f'o resultado da divisão dos numeros que voce me infformou é {div:.2f}')
+#devolvendo a informacao e deixando apenas 2 casas decimais
