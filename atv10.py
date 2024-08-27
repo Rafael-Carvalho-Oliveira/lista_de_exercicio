37ec6777f34e31a4158a4fb6de031ee09776a8df
tiagovinicius10 <168542213+tiagovinicius10@users.noreply.github.com>
2024-08-27 11:33:04 -0300

    atv10.py

--- atv10.py
+++ atv10.py
@@ -1,4 +1,4 @@
-nome = input('Digite seu nome: ')
-print(nome)
-print(type(nome))
+nome = input('Digite seu nome: ') #escrevendo seu nome
+print(nome) #informando o nome
+print(type(nome)) #informando o nome e o tipo de dado
  
